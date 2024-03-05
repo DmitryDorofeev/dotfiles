326e9048305fa72580301fd919c38d8f61ccf253
Dmitry Dorofeev <dorof21@yandex.ru>
2024-03-05 14:26:29 -0500

    fix: disable ctrl- in wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,6 +3,12 @@ local config = {}
 
 config.font = wezterm.font_with_fallback({ "JetBrains Mono", { family = "Symbols Nerd Font Mono", scale = 0.75 } })
 config.color_scheme = "Ayu Mirage"
-config.disable_default_key_bindings = true
+config.keys = {
+	{
+		key = "-",
+		mods = "CTRL",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+}
 
 return config
